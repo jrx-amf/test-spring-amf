1e44aa18605e836be334aa62ff8766f2bdb607e7
Antonio Fernández <antonio.fernandez@netscale.io>
2019-07-01 16:40:03 +0200

    added simple echo endpoint

--- src/main/java/com/example/testspringamf/DemoApplication.java
+++ src/main/java/com/example/testspringamf/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.testspringamf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +11,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/echo")
+	public String echo() {
+		return "echo";
+	}
 }
